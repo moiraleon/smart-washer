be659a49baf2261a14ebce44e6f213a75c93558f
Moira Leon <moira.leon2@gmail.com>
2021-09-16 18:10:13 -0600

    initial commit

---
+++ src/main/java/com/capstone/smartwasher/smartwasher/SmartWasherApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.smartwasher.smartwasher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWasherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWasherApplication.class, args);
+	}
+
+}
